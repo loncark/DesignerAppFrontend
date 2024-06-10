199a93f0c4b824428980e31a8aa0d5bbba125061
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-06-10 16:07:52 +0200

    Gemini works

--- src/api/GeminiApi.js
+++ src/api/GeminiApi.js
@@ -1,7 +1,18 @@
-import { query } from '../utils/requestService'
+import { BACKEND_BASE_URL } from '../utils/constants'
 
 export async function queryGemini(prompt) {
     const body = JSON.stringify({ prompt: prompt })
+    const response = await fetch(BACKEND_BASE_URL + 'gemini', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: body
+    });
+    
+    if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
 
-    return await query('gemini', 'POST', body);
+    return response.text();
 }
