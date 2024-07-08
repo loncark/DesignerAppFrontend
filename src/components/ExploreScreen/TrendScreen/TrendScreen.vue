7098f5a7160b3ba7ec6c182f58332e4ae98cc542
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-07-08 19:43:15 +0200

    Calendar -> InputText

--- src/components/ExploreScreen/TrendScreen/TrendScreen.vue
+++ src/components/ExploreScreen/TrendScreen/TrendScreen.vue
@@ -1,15 +1,15 @@
 <template>
     <div id="trendScreen" class="flex-column">
         <div class="searchBar flex-row">
-            <InputText v-model="country_code"/>
-            <Calendar v-model="date" iconDisplay="input" dateFormat="dd/mm/yy"/>
+            <InputText v-model="countryCode"/>
+            <InputText v-model="dateString"></InputText>
             <Button label="Search" icon="pi pi-search" @click="executeQuery"></Button>
         </div>
 
         <span class="loadingText" v-if="loading">Loading...</span>
         <div id="dateList" class="flex-column" v-else-if="!notFound">
             <div v-for="(date_item, index) in store.daily_searches" :key="index" class="dateItem flex-column">
-                <span class="dateText">{{ inverseFormatDate(date_item.date) }}</span>
+                <span class="dateText">{{ insertSlashesIntoDateString(date_item.date) }}</span>
                 <TrendListPerDate :searches="date_item.searches"/>
             </div>
         </div>
@@ -19,58 +19,42 @@
 
 <script setup>
 // trendovi mogu samo unazad mjesec dana
-import Calendar from 'primevue/calendar';
 import TrendListPerDate from './TrendListPerDate.vue';
 import { queryTrends } from '../../../api/TrendsApi';
 import { ref, onMounted } from 'vue';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import { useStore } from '../../../store/Store';
+import { convertDateToString, convertStringToDate, formatDateForBackend, insertSlashesIntoDateString } from '../../../utils/functions';
 
 const store = useStore();
 const date = ref(new Date());
-const country_code = ref('US')
-const next_date = ref(date);
+const dateString = ref(convertDateToString(date.value));
+const countryCode = ref('US');
+const nextDate = ref(date.value);
 const loading = ref(false);
 const notFound = ref(false);
 
 const executeQuery = async () => {
     try {
         loading.value = true;
-        const response = await queryTrends(formatDate(date.value), country_code.value);
+        const response = await queryTrends(formatDateForBackend(convertStringToDate(dateString.value)), countryCode.value);
         if (response.daily_searches) {
             store.daily_searches = response.daily_searches;
-            next_date.value = response.serpapi_pagination.next_date;
+            nextDate.value = response.serpapi_pagination.next_date;
             notFound.value = false;
         }
         else { 
             notFound.value = true;
         }
-        loading.value = false;
         
     } catch (error) {
         response.value = `Error: ${error.message}`;
     }
-};
-
-const formatDate = (date) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}${month}${day}`;
-};
-
-const inverseFormatDate = (dateString) => {
-    if (dateString.length !== 8) {
-        throw new Error("Invalid date string format. Expected 8 digits.");
+    finally {
+        loading.value = false;
     }
-
-    const year = dateString.slice(0, 4);
-    const month = dateString.slice(4, 6);
-    const day = dateString.slice(6, 8);
-
-    return `${year}/${month}/${day}`;
-}
+};
 
 onMounted(() => {
     if(store.daily_searches === null) {
@@ -88,7 +72,8 @@ onMounted(() => {
 .searchBar .p-component {
     height: 35px;
     text-align: center;
-    margin: auto;
+    margin-right: 10px;
+    margin-left: 0px;
 }
 
 .loadingText {
@@ -101,4 +86,5 @@ onMounted(() => {
     margin-top: 10px;
     margin-bottom: 10px;
 }
+
 </style>
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -16,6 +16,9 @@ export async function query(endpoint, method, body) {
     return await response.json();
 }
 
+
+//StableDiffusionScreen.vue
+
 export function base64ToBlob(base64) {
     let byteChars = atob(base64);
     let byteNumbers = new Array(byteChars.length);
@@ -33,4 +36,48 @@ export function blobToBase64(blob) {
       reader.onerror = error => reject(error);
       reader.readAsDataURL(blob);
     });
+}
+
+//TrendScreen.vue
+
+export const convertStringToDate = (dateString) => {
+    const [year, month, day] = dateString.split('/');
+  
+    const date = new Date(year, month - 1, day);
+    if (isNaN(date.getTime())) {
+      throw new Error('Invalid date');
+    }
+    
+    return date;
+  }
+  
+export const convertDateToString = (date) => {
+    if (!(date instanceof Date) || isNaN(date)) {
+      throw new Error('Invalid Date object');
+    }
+  
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+  
+    return `${year}/${month}/${day}`;
+  }
+
+export const formatDateForBackend = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}${month}${day}`;
+};
+
+export const insertSlashesIntoDateString = (dateString) => {
+    if (dateString.length !== 8) {
+        throw new Error("Invalid date string format. Expected 8 digits.");
+    }
+
+    const year = dateString.slice(0, 4);
+    const month = dateString.slice(4, 6);
+    const day = dateString.slice(6, 8);
+
+    return `${year}/${month}/${day}`;
 }
