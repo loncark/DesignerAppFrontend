eebb46ba829f42eaa61447f0b7bf1316ff0474bc
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-08-02 15:55:38 +0200

    added try-catch block

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,19 +1,24 @@
 import { BACKEND_BASE_URL, COUNTRY_CODES, TRADEMARK_FILTERS } from './constants'
 
 export async function query(endpoint, method, body) {
-    const response = await fetch(BACKEND_BASE_URL + endpoint, {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: body
-    });
+    try {
+        const response = await fetch(BACKEND_BASE_URL + endpoint, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: body
+        });
+        
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
     
-    if (!response.ok) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
+        return await response.json();
     }
-
-    return await response.json();
+    catch (error) {
+        console.log(error);
+    }
 }
 
 
