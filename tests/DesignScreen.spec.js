b1f9e519b45590494646e395aa2189f1bfc9d17e
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-08-05 11:52:57 +0200

    refactored

--- tests/DesignScreen.spec.js
+++ tests/DesignScreen.spec.js
@@ -7,40 +7,12 @@ import DesignCard from '../src/components/DesignScreen/DesignCard.vue'
 import { useStore } from '../src/store/Store'
 import { nextTick } from 'vue'
 import { getAllDesignsFromStorage } from '../src/api/FirebaseApi';
+import getAllDesignsFromStorageMockData from './mockdata/getAllDesignsFromStorageMockData';
 
 vi.mock('../src/api/FirebaseApi', () => ({
   getAllDesignsFromStorage: vi.fn(() => {
-    return Promise.resolve([{
-    "design_id": "94ece9bf-2057-49be-931c-ba06dcb1fe00",
-    "design_name": "Testin Stable Diffusion Steps",
-    "image_links": [
-      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/94ece9bf-2057-49be-931c-ba06dcb1fe00/08b52f49-9e6f-43ac-8df0-84f494a1cde0.png",
-      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/94ece9bf-2057-49be-931c-ba06dcb1fe00/2e8e94aa-6ed0-4194-abf6-0f72f29fe42d.png",
-      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/94ece9bf-2057-49be-931c-ba06dcb1fe00/2c3fed60-431a-48df-9eb6-158b955409b5.png",
-      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/94ece9bf-2057-49be-931c-ba06dcb1fe00/c746af4d-ba29-4f4e-9de6-872c36258ef1.png"
-    ],
-    "related_links": [],
-    "tags": [],
-    "title": "50, 100, 200, 400 steps"
-  },
-  {
-    "design_id": "a2ed1fc9-6034-404e-95da-447ecb5b01d5",
-    "design_name": "Holidays",
-    "image_links": [
-      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/a2ed1fc9-6034-404e-95da-447ecb5b01d5/f4b0ed6e-64f8-46e3-b65a-c12b3fbdc918.png",
-      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/a2ed1fc9-6034-404e-95da-447ecb5b01d5/2c47cd89-3b84-41e3-b5b8-072446c79137.png",
-      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/a2ed1fc9-6034-404e-95da-447ecb5b01d5/25ebde50-8664-4780-9b2a-743794186437.png",
-      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/a2ed1fc9-6034-404e-95da-447ecb5b01d5/4025159f-2cb4-4716-b301-bd43751ee48c.png"
-    ],
-    "related_links": [
-      "something.com"
-    ],
-    "tags": [
-      "christmas",
-      "easter"
-    ],
-    "title": "Holiday Shirts"
-  }])})
+    return Promise.resolve(getAllDesignsFromStorageMockData);
+  })
 }))
 
 describe('DesignScreen', () => {
@@ -88,9 +60,9 @@ describe('DesignScreen', () => {
   })
 
   it('updates store when handleDesignDeleted is called with current design ID', async () => {
-    const store = useStore()
-    store.design.design_id = '123'
-    await wrapper.vm.handleDesignDeleted('123')
-    expect(store.resetCreateScreen).toHaveBeenCalled()
+    const store = useStore();
+    store.design.design_id = '123';
+    await wrapper.vm.handleDesignDeleted('123');
+    expect(store.resetCreateScreen).toHaveBeenCalled();
   })
 })
---
+++ tests/mockdata/getAllDesignsFromStorageMockData.js
@@ -0,0 +1,31 @@
+export default [{
+    "design_id": "94ece9bf-2057-49be-931c-ba06dcb1fe00",
+    "design_name": "Testin Stable Diffusion Steps",
+    "image_links": [
+      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/94ece9bf-2057-49be-931c-ba06dcb1fe00/08b52f49-9e6f-43ac-8df0-84f494a1cde0.png",
+      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/94ece9bf-2057-49be-931c-ba06dcb1fe00/2e8e94aa-6ed0-4194-abf6-0f72f29fe42d.png",
+      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/94ece9bf-2057-49be-931c-ba06dcb1fe00/2c3fed60-431a-48df-9eb6-158b955409b5.png",
+      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/94ece9bf-2057-49be-931c-ba06dcb1fe00/c746af4d-ba29-4f4e-9de6-872c36258ef1.png"
+    ],
+    "related_links": [],
+    "tags": [],
+    "title": "50, 100, 200, 400 steps"
+  },
+  {
+    "design_id": "a2ed1fc9-6034-404e-95da-447ecb5b01d5",
+    "design_name": "Holidays",
+    "image_links": [
+      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/a2ed1fc9-6034-404e-95da-447ecb5b01d5/f4b0ed6e-64f8-46e3-b65a-c12b3fbdc918.png",
+      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/a2ed1fc9-6034-404e-95da-447ecb5b01d5/2c47cd89-3b84-41e3-b5b8-072446c79137.png",
+      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/a2ed1fc9-6034-404e-95da-447ecb5b01d5/25ebde50-8664-4780-9b2a-743794186437.png",
+      "https://storage.googleapis.com/designerapp-65092.appspot.com/images/a2ed1fc9-6034-404e-95da-447ecb5b01d5/4025159f-2cb4-4716-b301-bd43751ee48c.png"
+    ],
+    "related_links": [
+      "something.com"
+    ],
+    "tags": [
+      "christmas",
+      "easter"
+    ],
+    "title": "Holiday Shirts"
+  }]
