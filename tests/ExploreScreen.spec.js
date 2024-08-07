c16ad950a6b63e2febb4e80818b3efa2b21800a3
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-08-07 16:39:18 +0200

    NO NEED FOR STORE MOCKING

--- tests/ExploreScreen.spec.js
+++ tests/ExploreScreen.spec.js
@@ -1,38 +1,22 @@
-import { mount, shallowMount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import { describe, it, expect, beforeEach, vi } from 'vitest';
 import ExploreScreen from '../src/components/ExploreScreen/ExploreScreen.vue';
-import { useStore } from '../src/store/Store';
 import KeywordSearchScreen from '../src/components/ExploreScreen/KeywordSearchScreen/KeywordSearchScreen.vue';
 import EtsyScreen from '../src/components/ExploreScreen/EtsyScreen/EtsyScreen.vue';
 import TrendScreen from '../src/components/ExploreScreen/TrendScreen/TrendScreen.vue';
-import { createTestingPinia } from '@pinia/testing';
-import { setActivePinia, createPinia } from 'pinia'
-import TabMenu from 'primevue/tabmenu';
-import { nextTick, ref } from 'vue'
-import Dropdown from 'primevue/dropdown';
-import Calendar from 'primevue/calendar';
+import { nextTick } from 'vue'
 
 describe('ExploreScreen.vue', () => {
   let wrapper;
-  let store;
 
   beforeEach(() => {
-    const pinia = createPinia()
-    setActivePinia(createPinia())
-    store = useStore();
-
-    wrapper = mount(ExploreScreen, {
+    wrapper = shallowMount(ExploreScreen, {
         global: {
-          plugins: [pinia],
           stubs: {
               KeywordSearchScreen,
               EtsyScreen,
               TrendScreen,
-              ProgressSpinner: true,
-              InputText: true,
               TabMenu: true,
-              Dropdown: true,
-              Calendar: true,
             },
         }
       });
@@ -54,7 +38,7 @@ describe('ExploreScreen.vue', () => {
 
   it('initializes with the correct active tab', async () => {
     await nextTick();
-    expect(store.explore_active_tab).toBe(0);
+    expect(wrapper.vm.store.explore_active_tab).toBe(0);
   });
 
   it('updates the active tab when a tab is clicked', async () => {
